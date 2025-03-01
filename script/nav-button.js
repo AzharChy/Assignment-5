a49183fa333b6c96a6c81b7ef4d34bc38005bd4b
azhar-mahmud-chowdhury <azharchowdhury075@gmail.com>
2025-03-01 19:49:10 +0600

    3rd commit

--- script/nav-button.js
+++ script/nav-button.js
@@ -1,6 +1,6 @@
 document.getElementById("nav-btn").addEventListener("click",function(event){
     event.preventDefault();
-    let colors = ["orange", "blue", "pink", "white", "black", "red"];
+    let colors = ["orange", "blue", "pink", "black", "red"];
     let randomColor = Math.floor(Math.random() * colors.length);
     document.querySelector(".body").style.backgroundColor = colors[randomColor];
 
